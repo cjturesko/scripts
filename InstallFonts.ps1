53741711bb49075035753682ce8b284113ad6d0a
cjturesko <cjturesko@gmail.com>
2024-11-05 09:47:14 -0500

    Add files via upload

---
+++ InstallFonts.ps1
@@ -0,0 +1,10 @@
+echo copying Fonts over
+$FontFolder = "D:\New_Computer_Software\~Fonts"
+$FontItem = Get-Item -Path $FontFolder
+$FontList = Get-ChildItem -Path "$FontItem\*" -Include ('*.fon','*.otf','*.ttc','*.ttf')
+
+foreach ($Font in $FontList) {
+        Write-Host 'Installing font -' $Font.BaseName
+        Copy-Item $Font "C:\Windows\Fonts"
+        New-ItemProperty -Name $Font.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $Font.name         
+}
